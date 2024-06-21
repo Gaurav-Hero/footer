ab56db0f0e03948e29546e691bf4fd6ee9fb5f4f
Gaurav Sharma <129644605+Gaurav-Hero@users.noreply.github.com>
2024-06-21 07:05:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import UpperFooter from './components/UpperFooterPart';
+function App() {
+  return (
+    <footer>
+        <UpperFooter/>
+    </footer>
+  )
+}
+
+export default App
---
+++ src/components/UpperFooterPart.jsx
@@ -0,0 +1,60 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function UpperFooterPart() {
+  return (
+    <div className='upperFooterPart'>
+      <div className="upperFooterPart-1">
+        <div className="upf1">
+            <h3>Who are we ?</h3>
+            <ul>
+                <li className="footerList">Our Team</li>
+                <li className="footerList">Press</li>
+                <li className="footerList">Authenticity</li>
+                <li className="footerList">Careers CSR</li>
+                <li className="footerList">Divueens</li>
+                <li className="footerList">Investor Relation</li>
+            </ul>
+        </div>
+        <div className="upf2">
+        <h3>Help</h3>
+            <ul>
+                <li className="footerList">Contact Us</li>
+                <li className="footerList">Frequently Asked Questions</li>
+                <li className="footerList">Cancellation and Return</li>
+                <li className="footerList">Shipping and Delivery </li>
+                <li className="footerList">Sell on Divueens</li>
+                <li className="footerList">Privacy Policy</li>
+            </ul>
+        </div>
+        <div className="upf3">
+        <h3>INSPIRE US </h3>
+            <ul>
+                <li className="footerList">Beauty Books</li>
+                <li className="footerList">Divueens Network</li>
+            </ul>
+        </div>
+        <div className="upf4">
+        <h3>QUICK LINKS</h3>
+            <ul>
+                <li className="footerList">Special Offers</li>
+                <li className="footerList">Trendings</li>
+                <li className="footerList">New Launches</li>
+                <li className="footerList">Divueens Fashion</li>
+            </ul>
+        </div>
+        <div className="upf5">
+        <h3>TOP CATEGORIES</h3>
+            <ul>
+                <li className="footerList">Makeup</li>
+                <li className="footerList">Hair</li>
+                <li className="footerList">Organics</li>
+            </ul>
+        </div>
+      </div>
+      <div className="upperFooterPart-2"></div>
+    </div>
+  )
+}
+
+export default UpperFooterPart
